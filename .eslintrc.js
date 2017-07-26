830998d6c66a5d00e83d74626e631a5e1b06b427
Corey600 <fcx600@163.com>
2017-07-26 16:16:24 +0800

    Add config files.

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,12 +8,18 @@ module.exports = {
   "rules": {
     "linebreak-style": ["error", "unix"],
     "strict": ["off", "global"],
+    "jsx-a11y/img-has-alt": ["off"],
+    "react/jsx-space-before-closing": ["off"],
+    "react/jsx-tag-spacing": ["error", {"beforeSelfClosing": "always"}],
+    "react/jsx-filename-extension": ["warn", {"extensions": [".js", ".jsx"]}],
   },
   "plugins": [
-    "react"
+    "import",
+    "react",
+    "jsx-a11y",
   ],
   "globals": {
     "it": true,
-    "describe": true
+    "describe": true,
   }
 };
